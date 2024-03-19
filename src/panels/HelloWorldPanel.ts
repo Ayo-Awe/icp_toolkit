a5b7810af752e69ba845ae1a0cd19b4631aeabb9
Awe Ayomidipupo <pupoawe@gmail.com>
2024-03-19 16:58:55 +0100

    refact: switch to react webview dev setup

---
+++ src/panels/HelloWorldPanel.ts
@@ -0,0 +1,97 @@
+import {
+  Disposable,
+  Webview,
+  WebviewPanel,
+  window,
+  Uri,
+  ViewColumn,
+  WebviewViewProvider,
+  WebviewView,
+  WebviewViewResolveContext,
+  CancellationToken,
+  SnippetString,
+} from "vscode";
+import { getUri } from "../utilities/getUri";
+import { getNonce } from "../utilities/getNonce";
+
+/**
+ * This class manages the state and behavior of HelloWorld webview panels.
+ *
+ * It contains all the data and methods for:
+ *
+ * - Creating and rendering HelloWorld webview panels
+ * - Properly cleaning up and disposing of webview resources when the panel is closed
+ * - Setting the HTML (and by proxy CSS/JavaScript) content of the webview panel
+ * - Setting message listeners so data can be passed between the webview and extension
+ */
+export class ICPWebViewProvider implements WebviewViewProvider {
+  public static readonly viewType = "canisters";
+
+  private _view?: WebviewView;
+
+  constructor(private readonly _extensionUri: Uri) {}
+
+  public resolveWebviewView(
+    webviewView: WebviewView,
+    context: WebviewViewResolveContext,
+    _token: CancellationToken
+  ) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview, this._extensionUri);
+
+    webviewView.webview.onDidReceiveMessage((data) => {
+      switch (data.type) {
+        case "colorSelected": {
+          window.activeTextEditor?.insertSnippet(new SnippetString(`#${data.value}`));
+          break;
+        }
+      }
+    });
+  }
+
+  /**
+   * Defines and returns the HTML that should be rendered within the webview panel.
+   *
+   * @remarks This is also the place where references to the React webview build files
+   * are created and inserted into the webview HTML.
+   *
+   * @param webview A reference to the extension webview
+   * @param extensionUri The URI of the directory containing the extension
+   * @returns A template string literal containing the HTML that should be
+   * rendered within the webview panel
+   */
+  private _getHtmlForWebview(webview: Webview, extensionUri: Uri) {
+    // The CSS file from the React build output
+    const stylesUri = getUri(webview, extensionUri, ["webview-ui", "build", "assets", "index.css"]);
+    // The JS file from the React build output
+    const scriptUri = getUri(webview, extensionUri, ["webview-ui", "build", "assets", "index.js"]);
+
+    const nonce = getNonce();
+
+    // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
+    return /*html*/ `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+          <link rel="stylesheet" type="text/css" href="${stylesUri}">
+          <title>Hello World</title>
+        </head>
+        <body>
+          <div id="root"></div>
+          <script type="module" nonce="${nonce}" src="${scriptUri}"></script>
+        </body>
+      </html>
+    `;
+  }
+}
--- src/provider.ts
+++ src/provider.ts
@@ -17,7 +17,9 @@ export class CanisterProvider implements vscode.TreeDataProvider<Canister> {
     }
 
     if (!element) {
-      const child = spawn("dfx",["canister", "status", "--all"] { cwd: this.workspaceRoot });
+      const child = spawn("dfx", ["canister", "status", "--all"], {
+        cwd: this.workspaceRoot,
+      });
 
       child.stdout.on("data", (data) => {
         console.log(`stdout:\n${data}`);
@@ -39,22 +41,10 @@ export class CanisterProvider implements vscode.TreeDataProvider<Canister> {
   private getDepsInPackageJson(packageJsonPath: string): Canister[] {
     if (this.pathExists(packageJsonPath)) {
       const toDep = (moduleName: string, version: string): Canister => {
-        if (
-          this.pathExists(
-            path.join(this.workspaceRoot, "node_modules", moduleName)
-          )
-        ) {
-          return new Canister(
-            moduleName,
-            version,
-            vscode.TreeItemCollapsibleState.Collapsed
-          );
+        if (this.pathExists(path.join(this.workspaceRoot, "node_modules", moduleName))) {
+          return new Canister(moduleName, version, vscode.TreeItemCollapsibleState.Collapsed);
         } else {
-          return new Canister(
-            moduleName,
-            version,
-            vscode.TreeItemCollapsibleState.None
-          );
+          return new Canister(moduleName, version, vscode.TreeItemCollapsibleState.None);
         }
       };
 
@@ -84,6 +74,113 @@ export class CanisterProvider implements vscode.TreeDataProvider<Canister> {
     }
     return true;
   }
+}
+
+export class ICPWebViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "canisters";
+
+  private _view?: vscode.WebviewView;
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    webviewView.webview.onDidReceiveMessage((data) => {
+      switch (data.type) {
+        case "colorSelected": {
+          vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+          break;
+        }
+      }
+    });
+  }
+
+  public addColor() {
+    if (this._view) {
+      this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+      this._view.webview.postMessage({ type: "addColor" });
+    }
+  }
+
+  public clearColors() {
+    if (this._view) {
+      this._view.webview.postMessage({ type: "clearColors" });
+    }
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+    );
+
+    // Do the same for the stylesheet.
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+    );
+
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+				<html lang="en">
+				<head>
+					<meta charset="UTF-8">
+
+					<!--
+						Use a content security policy to only allow loading styles from our extension directory,
+						and only allow scripts that have a specific nonce.
+						(See the 'webview-sample' extension sample for img-src content security policy examples)
+					-->
+					<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+					<link href="${styleResetUri}" rel="stylesheet">
+					<link href="${styleVSCodeUri}" rel="stylesheet">
+					<link href="${styleMainUri}" rel="stylesheet">
+
+					<title>Cat Colors</title>
+				</head>
+				<body>
+					<ul class="color-list">
+					</ul>
+
+					<button class="add-color-button">Add Color</button>
+
+					<script nonce="${nonce}" src="${scriptUri}"></script>
+				</body>
+				</html>`;
+  }
+}
+
+function getNonce() {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
 
 class Canister extends vscode.TreeItem {
